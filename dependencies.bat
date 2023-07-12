e46995285626aa7a93e693c47cc14474575fddff
YuriXbr <claudemiranjos67@gmail.com>
2023-07-12 04:59:55 -0300

    🐞 V1.5.6 | Bug fix. (Chalk Solucionado & +)
    Corrigido problema com a dependencia Chalk.
    * A versão instalada ao usar o NPM i era imcompativel com o projeto.
    * Removido Verificação de instalação do Node.js já que era inutil verificar isso com o próprio node
    * Atualizado o package.json para garantir o funcionamento das dependencias.
    * Atualizado start.bat e dependencies.bat para facilitar a instalação, execução do app e identificação de erros.
    * Atualizado o dependencies.bat para ele funcionar

--- dependencies.bat
+++ dependencies.bat
@@ -1,27 +1,3 @@
 @echo off
-echo.
-echo Instalando Dependencias do ConfigManager.
 
-NET SESSION >NUL 2>&1
-IF %ERRORLEVEL% EQU 0 (
-    
-set "dir=%~dp0"
-
-cd %dir%
 npm i
-
-echo dependencias Instaladas
-pause
-exit
-
-
-) ELSE (
-    :: Não está sendo executado como administrador, solicita permissões de administrador
-    echo Por favor, de permissao de administrador para que as dependencias do programa sejam instaladas corretamente
-    echo (Por motivos de segurança, você não verá a senha sendo digitada)
-    set "script=%~0"
-    %windir%\system32\runas.exe /user:%USERDOMAIN%\%USERNAME% "%script%"
-    exit /b
-)
-
-exit
